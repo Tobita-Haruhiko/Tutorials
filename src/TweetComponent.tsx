1d46323bfe242a73492006238048e24ad8e851f5
Tobita-Haruhiko <period9801@gmail.com>
2024-06-17 18:06:32 +0900

    useRefをつかって、色が保存されるようにしました

--- src/TweetComponent.tsx
+++ src/TweetComponent.tsx
@@ -1,13 +1,14 @@
-import { useMemo } from 'react';
+import { useRef } from 'react';
 type tweetContainer = {user: string, text: string};
 
 export const TweetComponent = ({posts, num}:{posts: tweetContainer, num: number}) => {
-    const rand = useMemo(() => {return(Math.floor(Math.random() * 3))},[])
-    
-    console.log('num: ' + num + ', rand:' + Number(rand))
+    const randRef = useRef<number>(Math.floor(Math.random() * 3))
+
+    console.log('num: ' + num + ', rand:' + Number(randRef.current))
+    console.log(typeof randRef)
 
     return (
-      <div className={'color' + rand}>
+      <div className={'color' + randRef.current}>
         <div className='post'>
           <a href="#"><div className="userIcon">{posts.user.charAt(0)}</div></a>
           <div className="postText">
